cfd26a326e21068f1c2a7af81da45edb346c009d
woomurf <dngusdnd@gmail.com>
2022-02-19 20:38:15 +0900

    fix: update user check in authValidator

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,17 +2,14 @@ const jwt = require('jsonwebtoken');
 const { USER } = require("../models");
 const { ACCESS_TOKEN_SECRET } = require('../const');
 
-const _checkUserExist = async (email) => {
+const _getUserInstance = async (email) => {
   const user = await USER.findOne({
     where: {
       email,
     }
   });
 
-  if (user) {
-    return true;
-  }
-  return false;
+  return user || null;
 }
 
 const registerValidator = async (req, res, next) => {
@@ -23,7 +20,7 @@ const registerValidator = async (req, res, next) => {
     });
   }
 
-  const isExist = await _checkUserExist(email);
+  const isExist = await _getUserInstance(email);
   if (isExist) {
     return res.status(409).json({
       message: 'email already used.',
@@ -43,7 +40,8 @@ const authValidator = async (req, res, next) => {
 
   try {
     const decodedToken = await jwt.verify(token, ACCESS_TOKEN_SECRET);
-    res.locals.id = decodedToken.id;
+    const user = _getUserInstance(decodedToken.id);
+    res.locals.user = user;
   } catch (error) {
     return res.status(500).json({
       message: 'Failed to verify token',
