e60d30651a2a3b0f38ac179cbf5b10b698ffb172
Ohneul <zv9612@gmail.com>
2022-03-27 19:03:01 +0900

    Update MemberFrame prepare API and Add memberInfoPopup

---
+++ frontend/src/component/MemberBox.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+import DefaultProfileImage from "../image/DefaultProfileImage.png";
+import '../scss/page.scss';
+
+class MemberBox extends Component {
+  render() {
+    return (
+      <div>
+        <div className="memberBodyDiv">
+          <div className="memberBody">
+            <img alt="" src={this.props.profileImage||DefaultProfileImage} id="memberProfileImage"/>
+            <div className="memberName">
+              {this.props.name}
+            </div>
+          </div>
+          <button 
+            className="settingButton"
+            onClick={function(){
+              this.props.onClick(this.props.name, this.props.profileImage, this.props.birthday);
+            }.bind(this)}
+          >
+            정보
+          </button>
+        </div>
+        {this.getDivLine(this.props.lastIndexYn)}
+      </div>
+    );
+  }
+
+  getDivLine(divLineYn) {
+    var divLine = null;
+    if (!divLineYn) {
+      divLine = <div className="memberLine"/>;
+    }
+    return divLine;
+  }
+};
+
+export default MemberBox;
--- frontend/src/screen/MemberFrame.js
+++ frontend/src/screen/MemberFrame.js
@@ -1,14 +1,47 @@
 import React, {Component} from "react";
-import '../scss/page.scss';
-import Sky from '../image/Sky.png';
 import SearchBox from "../component/SearchBox";
+import MemberBox from "../component/MemberBox";
+import MemberInfoPopup from "../popup/MemberInfoPopup";
+import '../scss/page.scss';
+import axios from "axios";
 
 class MemberFrame extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      memberInfo :{
+        name:"",
+        image:"",
+        birthday:""
+      },
+      members :[{
+        name:"권영준",
+        birthday:"19961000"
+      }, {
+        name:"우현웅",
+        birthday:"19961100"
+      }, {
+        name:"정의창",
+        birthday:"19961200"
+      }, {
+        name:"가상인물",
+        birthday:"19480202"
+      }, {
+        name:"가장인물",
+        birthday:"19430204"
+      }, {
+        name:"가정인물",
+        birthday:"19520207"
+      }]
     }
+  }
+
+  async componentDidMount(){
+    await axios.get('/member/list')
+    .then(function(res){
+      console.log(res.data[0]);
+      this.setState({member:res.data});
+    }.bind(this));
   }
 
   render() {
@@ -18,54 +51,47 @@ class MemberFrame extends Component {
           <SearchBox
             label="멤버 검색"
           />
-          {/* API 떠서 memberFrame 컴포넌트로 따로 제외 후 
-          API로 받아온 카운트로 멤버 인원 입력,
-          카운트에 맞게 map 사용으로 멤버 출력,
-          SearchBox로 멤버 검색 기능 구현 */}
+          {/* SearchBox로 멤버 검색 기능 구현 */}
           <div className="memberFrameBody">
             <div className="memberHeader">
-              멤버 3
+              멤버 {this.state.members.length}
             </div>
             <div className="settingLine"/>
-            <div className="memberBodyDiv">
-              <div className="memberBody">
-                <img alt="" src={Sky} id="memberProfileImage"/>
-                <div className="memberName">
-                  권영준
-                </div>
-              </div>
-              <button className="settingButton">
-                설정
-              </button>
-            </div>
-            <div className="memberLine"/>
-            <div className="memberBodyDiv">
-              <div className="memberBody">
-                <img alt="" src={Sky} id="memberProfileImage"/>
-                <div className="memberName">
-                  우현웅
-                </div>
-              </div>
-              <button className="settingButton">
-                설정
-              </button>
-            </div>
-            <div className="memberLine"/>
-            <div className="memberBodyDiv">
-              <div className="memberBody">
-                <img alt="" src={Sky} id="memberProfileImage"/>
-                <div className="memberName">
-                  정의창
-                </div>
-              </div>
-              <button className="settingButton">
-                설정
-              </button>
-            </div>
+            {this.state.members.map((member,index) => {
+              return(
+                <MemberBox
+                  key={index}
+                  name={member.name}
+                  profileImage={member.profileImage}
+                  birthday={member.birthday}
+                  lastIndexYn={this.state.members.length === index + 1}
+                  onClick={function(nameInfo, imageInfo, birthdayInfo) {
+                    this.setState({
+                      memberInfo:{
+                        name:nameInfo,
+                        image:imageInfo,
+                        birthday:birthdayInfo
+                      }
+                    }); 
+                    this.showMemberInfoPopup();
+                  }.bind(this)}
+                />
+              )
+            })}
           </div>
         </div>
+        <MemberInfoPopup
+          name={this.state.memberInfo.name}
+          image={this.state.memberInfo.image}
+          birthday={this.state.memberInfo.birthday}
+        />
       </div>
     );
+  }
+
+  showMemberInfoPopup() {
+    const memberInfoPopup = document.querySelector('#memberInfoPopup');
+    memberInfoPopup.classList.remove('hide');
   }
 };
 
